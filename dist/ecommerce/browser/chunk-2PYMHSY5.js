bd5771f63ede8226ff9b1664d542a06b13bb7290
Ajmal-noor01 <ajmalnoormohamed@gmail.com>
2025-04-02 12:21:07 +0530

    added redirects file

---
+++ dist/ecommerce/browser/chunk-2PYMHSY5.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-QOBWZXRX.js";import"./chunk-KEKFTDOE.js";import"./chunk-7HBAQUPH.js";import"./chunk-QBOCU6I6.js";export{a as FooterComponent};
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,5 +6,6 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimations(), provideToastr()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+  provideRouter(routes), provideAnimations(), provideToastr()]
 };
